de405eb89e0305c52bf6cfd6ed81fda64ecb8564
Michael Almeida <michaelralmeida@gmail.com>
2020-07-16 13:11:11 -0300

    New port version

---
+++ precache-manifest.92e3aa6daae98a5c8118c86a5691e1a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b63dea890f3b1eb8955",
+    "url": "/static/js/main.cbea1587.chunk.js"
+  },
+  {
+    "revision": "3f8f3bd0d6c5dad1c388",
+    "url": "/static/js/2.66c789a0.chunk.js"
+  },
+  {
+    "revision": "0b63dea890f3b1eb8955",
+    "url": "/static/css/main.e0bba570.chunk.css"
+  },
+  {
+    "revision": "659ae8e77be0d2aa5334b557685ed113",
+    "url": "/index.html"
+  }
+];
